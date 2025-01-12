cdcd0c1f9ed192058a66ab1e58afcbaf06129bd4
Daniel Kimani <kimdan2030@gmail.com>
2025-01-12 14:26:08 +0300

    Acknowledge

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -157,8 +157,8 @@
 						</div>
 						  <div class="card-body">
 							<div class="d-flex align-items-center ms-auto font-13 gap-2 mb-3">
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #14abef"></i>Payment</span>
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #ffc107"></i>Disbursement</span>
+								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #009E60"></i>Payment</span>
+								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #ffa500"></i>Disbursement</span>
 							</div>
 							<div class="chart-container-1">
 								<canvas id="chart1"></canvas>
